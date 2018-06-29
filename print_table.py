2709d6e32d1e2f7795bc823d64ef796fd1b3951c
Robert Bickley <robertbickley@Roberts-MacBook-Pro.local>
2018-06-29 12:21:18 -0400

    Added the print_table.py file

---
+++ print_table.py
@@ -0,0 +1,8 @@
+#Accepts a dictionary as an input, prints the contents as a rich table
+
+def print_table(dict):
+    #-Construct an empty table of dimensions 2xn
+    #where n is the number of entries in the dictionary
+    #-Fill the top cells with the keys of the dictionary
+    #-Fill the bottom cells with the values of the dictionary
+    #-Neaten the table by making sure all cells are of uniform size
