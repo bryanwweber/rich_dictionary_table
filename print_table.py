66c58c20d5ab08c550b512901d2740a2e218caf1
Robert Bickley <robertbickley@Roberts-MacBook-Pro.local>
2018-06-29 12:32:32 -0400

    Added skeleton code for filling in the table

--- print_table.py
+++ print_table.py
@@ -1,8 +1,16 @@
 #Accepts a dictionary as an input, prints the contents as a rich table
 
 def print_table(dict):
-    #-Construct an empty table of dimensions 2xn
+    #Construct an empty table of dimensions 2xn
+    n = len(dict)
+    table = ""
+    for i in n:
+        #Construct the empty HTML table
     #where n is the number of entries in the dictionary
-    #-Fill the top cells with the keys of the dictionary
-    #-Fill the bottom cells with the values of the dictionary
+    #Fill the top cells with the keys of the dictionary
+    for key in dict:
+        #add each key
+    #Fill the bottom cells with the values of the dictionary
+    for key in dict:
+        #add each dict[key]
     #-Neaten the table by making sure all cells are of uniform size
