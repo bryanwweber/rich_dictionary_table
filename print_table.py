2ed3280809a47aa4167fb449355ac748173fc6bb
Robert Bickley <rbickley99@gmail.com>
2018-07-03 12:07:25 -0400

    Changed a variable name so as to not compete with Python's built-in dict variable

--- print_table.py
+++ print_table.py
@@ -1,10 +1,10 @@
 #Accepts a dictionary as an input, outputs the contents as a rich table
 
-def print_table(dict, col_1, col_2):
+def print_table(user_dict, col_1, col_2):
     import html
     #Create a list of lists for the dictionary
     table_data = [[col_1, col_2]]
-    for key in dict:
+    for key in user_dict:
         table_data.append([key, dict[key]])
     print(table_data)
 
